9859b59ae46c78d6650bdb43cc29813916b0e2f2
Matias <mtsbrr07@gmail.com>
2024-11-15 16:49:30 +0100

    Agregar servicio de autenticación con métodos para login y registro de usuarios

--- TypeScript/Proyecto UD1/FoodScore/src/classes/auth-service.ts
+++ TypeScript/Proyecto UD1/FoodScore/src/classes/auth-service.ts
@@ -1,3 +1,30 @@
-export class AuthService {
+import { SERVER, USER_LOGIN_ENDPOINT, USER_REGISTER_ENDPOINT} from '../constants';
+import { Http } from './http';
+import { UserLogin, User } from '../interfaces/user';
 
+
+export class AuthService {
+    private _http: Http;
+
+    constructor() {
+        this._http = new Http();
+    }
+
+    async login(userLogin: UserLogin): Promise<void> {
+        try {
+            await this._http.post(`${SERVER}${USER_LOGIN_ENDPOINT}`, userLogin);
+        } catch (error) {
+            console.error('Error al hacer login:', error);
+            throw error;
+        }
+    }
+
+    async register(userInfo: User): Promise<void> {
+        try {
+            await this._http.post(`${SERVER}${USER_REGISTER_ENDPOINT}`, userInfo);
+        } catch (error) {
+            console.error('Error al registrar usuario:', error);
+            throw error;
+        }
+    }
 }
--- TypeScript/Proyecto UD1/FoodScore/src/constants.ts
+++ TypeScript/Proyecto UD1/FoodScore/src/constants.ts
@@ -2,5 +2,7 @@
 // export const SERVER = 'http://localhost:3000';
 export const SERVER = 'https://api.fullstackpro.es/foodscore-lite';
 export const RESTAURANTS_ENDPOINT = '/restaurants';
+export const USER_LOGIN_ENDPOINT = '/auth/login';
+export const USER_REGISTER_ENDPOINT = '/auth/register';
 //const MAX_IMAGE_SIZE = 1024 * 1024; // 1MB
 //const ALLOWED_IMAGE_TYPES = ['image/jpeg', 'image/png', 'image/gif'];
